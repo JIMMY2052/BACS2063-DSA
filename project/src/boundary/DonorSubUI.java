03b5ae2e4a487bb3ec5e564c087fed756cc962db
jhnthnljyng <92502880+jhnthnljyng@users.noreply.github.com>
2024-08-27 18:41:15 +0800

    donor ui

---
+++ project/src/boundary/DonorSubUI.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package boundary;
+
+/**
+ *
+ * @author User
+ */
+public class DonorSubUI {
+    public void donorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|  1. Add Donor                                |\n" +
+                           "|  2. Remove Donor                             |\n" +
+                           "|  3. Update Donor Details                     |\n" +
+                           "|  4. Search Donor Details                     |\n" +
+                           "|  5. List Donors With All the Donations made  |\n" +
+                           "|  6. Filter Donor based On Criteria           |\n" +
+                           "|  7. Categories Donors                        |\n" +
+                           "|  8. Generate Donor Summary Report            |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void addDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|                                              |\n" +
+                           "|                Add a New Donor               |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void removeDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|                                              |\n" +
+                           "|                Remove a Donor                |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void updateDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|                                              |\n" +
+                           "|             Update Donor Details             |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void searchDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" + 
+                           "|                                              |\n" +
+                           "|               Search a New Donor             |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void listDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|                                              |\n" +
+                           "|   List Donors With All the Donations made    |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void filterDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|                                              |\n" +
+                           "|        Filter Donor based On Criteria        |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void categoriesDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|                                              |\n" +
+                           "|              Categories Donor                |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+    
+    public void reportDonorMenu() {
+        System.out.println("+----------------------------------------------+\n" +
+                           "|                                              |\n" +
+                           "|         Generate Donor Summary Report        |\n" +
+                           "|                                              |\n" +
+                           "+----------------------------------------------+\n");
+    }
+}
