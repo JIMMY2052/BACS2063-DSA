d3d22be0a26bf536d28fe84bb4b606d4755f1268
JIMMY2052 <jimmychankahlok66@gmail.com>
2024-09-02 01:54:21 +0800

    Update SortedArrayList.java

--- project/src/adt/SortedArrayList.java
+++ project/src/adt/SortedArrayList.java
@@ -80,14 +80,7 @@ public class SortedArrayList<T extends Comparable<T>> implements SortedListInter
 
     }
 
-    private int getElementIndex(T object) {
-        for (int i = 0; i < numberOfEntries; i++) {
-            if (array[i].equals(object)) {
-                return i;
-            }
-        }
-        return -1;
-    }
+
 
     // Clear the list
     @Override
